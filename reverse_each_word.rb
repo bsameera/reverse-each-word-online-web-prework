f36fbf8267a1ad4afce04610aabef29cbf199dfe
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-08 04:04:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   #convert the string into an Array 
   str_arr = string.split('')
-  reversed = str_arr.collect do |word|
+  str_arr.collect do |word|
     word.reverse!
   end
   return reversed.join('')
